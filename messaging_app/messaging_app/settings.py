f9621fd4f7b087e2559f7f8c7e05c51da1986f26
Simon Kimotho <kimothokarani@gmail.com>
2025-07-20 01:20:22 +0300

    Add permission and authentication classes

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -122,3 +122,16 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.conitrib.auth` permssions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        # You might also add other authentication methods here later,
+        # like TokenAuthentication or JWTAuthentication.
+    ]
+}
