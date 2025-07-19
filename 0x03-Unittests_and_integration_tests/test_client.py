6141c932c7ed6c4d5b3fa92dac4c401415088c1a
Simon Kimotho <kimothokarani@gmail.com>
2025-07-20 00:19:10 +0300

    Unit test for a static method, using parameterization to check both a success case and a failure case

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -6,6 +6,7 @@ import unittest
 from unittest.mock import patch, PropertyMock, MagicMock
 from parameterized import parameterized
 from client import GithubOrgClient
+from typing import Dict
 
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -70,6 +71,22 @@ class TestGithubOrgClient(unittest.TestCase):
 
             mock_public_repos_url.assert_called_once()
             mock_get_json.assert_called_once()
+
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False),
+    ])
+    def test_has_license(
+            self,
+            repo: Dict,
+            license_key: str,
+            expected: bool
+    ) -> None:
+        """
+        Tests the `has_license` static method with different inputs.
+        """
+        result = GithubOrgClient.has_license(repo, license_key)
+        self.assertEqual(result, expected)
 
 
 if __name__ == '__main__':
