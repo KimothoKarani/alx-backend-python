0dd2684304c09770289a623c400026ff26ac74b1
Simon Kimotho <kimothokarani@gmail.com>
2025-08-10 17:11:34 +0300

    feat: Containerize Django messaging app with Docker

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'corsheaders',  # If your frontend is on a different domain/port
     'django_filters',  # <-- ADD THIS LINE
+    'django_extensions',
 
 ]
 
